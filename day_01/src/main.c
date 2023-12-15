e69d1fb4c725449fe5e23d3c867dda2493810dfa
EcklerOChokola <105422409+EcklerOChokola@users.noreply.github.com>
2023-12-15 20:41:38 +0100

    Day 01

--- day_01/src/main.c
+++ day_01/src/main.c
@@ -9,9 +9,42 @@ int main(int argc, char **argv) {
 	} else {
 		file = fopen("input.txt", "r");
 	}
+
+	char line[8];
+	char found = 0;
+	char found_two = 0;
+	int values[256];
+	int cursor = 0;
 	
 	while(!feof(file)) {
-		// Code goes here
+		fgets(line, 16, file);
+
+		values[cursor] = atoi(line);
+		cursor++;
+	}
+
+	for (int i = 0; i < cursor; i++)
+	{
+		for (int j = i; j < cursor; j++)
+		{
+			for (int k = 0; (!found_two && (k < cursor)); k++)
+			{
+				if (values[i] + values[j] + values[k] == 2020)
+				{
+					printf("Part 2 : %d\n", values[i] * values[j] * values[k]);
+					found_two++;
+					break;
+				}
+				
+			}
+			
+			if (values[i] + values[j] == 2020) {
+				printf("Part 1 : %d\n", values[i] * values[j]);
+				found++;
+				break;
+			}
+		}
+		if (found && found_two) break;
 	}
 	
 	fclose(file);
