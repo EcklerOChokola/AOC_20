210d375ed75fd078e714fb2e526e9f286f674e3f
EcklerOChokola <105422409+EcklerOChokola@users.noreply.github.com>
2023-12-15 21:24:26 +0100

    Day 03

--- day_03/src/main.c
+++ day_03/src/main.c
@@ -10,9 +10,40 @@ int main(int argc, char **argv) {
 		file = fopen("input.txt", "r");
 	}
 	
+	unsigned long count_one = 0;
+	unsigned long count_two = 0;
+	unsigned long count_three = 0;
+	unsigned long count_five = 0;
+	unsigned long count_seven = 0;
+	int x = 1;
+
+	// Max length in input is 31 + CR + null
+	char line[36];
+
+	// dump first line
+	fgets(line, 36, file);
+
+	// Copied from Windows, discard 1 for CR and 1 because of null termination
+	int mod = strlen(line) - 2;
+
 	while(!feof(file)) {
-		// Code goes here
+		fgets(line, 36, file);
+		if (line[(x)%mod] == '#') count_one++;
+		if (line[(x*3)%mod] == '#') count_three++;
+		if (line[(x*5)%mod] == '#') count_five++;
+		if (line[(x*7)%mod] == '#') count_seven++;
+		if (line[(x/2)%mod] == '#' && (x%2 == 0)) count_two++;
+		x++;
 	}
+
+	//printf("%lu\n", count_one);
+	//printf("%lu\n", count_two);
+	//printf("%lu\n", count_three);
+	//printf("%lu\n", count_five);
+	//printf("%lu\n", count_seven);
+
+	printf("Part 1 : %lu\n", count_three);
+	printf("Part 2 : %lu\n", count_one * count_three * count_five * count_seven * count_two);
 	
 	fclose(file);
 	return 0;
