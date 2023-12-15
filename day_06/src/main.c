9c745cadf3a948b24f8a467e1ae0ca265d4ad9a5
EcklerOChokola <105422409+EcklerOChokola@users.noreply.github.com>
2023-12-16 00:58:33 +0100

    Day 06

--- day_06/src/main.c
+++ day_06/src/main.c
@@ -2,6 +2,18 @@
 #include <stdlib.h>
 #include <string.h>
 
+int cond_sum(int* array, int len, int value) {
+	int total = 0;
+	for (int i = 0; i < len; i++) {
+		if (array[i] >= value) total++;
+	}
+	return total;
+}
+
+void init_table(int* array, int len, int value) {
+	for (int i = 0; i < len; i++) array[i] = value;
+}
+
 int main(int argc, char **argv) {
 	FILE* file;
 	if (argc > 1) {
@@ -9,11 +21,41 @@ int main(int argc, char **argv) {
 	} else {
 		file = fopen("input.txt", "r");
 	}
+
+	int total_one = 0;
+	int total_two = 0;
+	int answerers = 0;
+	char line[32];
+	int answered[26];
+	init_table(answered, 26, 0);
 	
 	while(!feof(file)) {
-		// Code goes here
-	}
+		fgets(line, 32, file);
+		// change to 1 if on Linux
+		if (strlen(line) == 2) {
+			total_one += cond_sum(answered, 26, 1);
+			total_two += cond_sum(answered, 26, answerers);
+			init_table(answered, 26, 0);
+			answerers = 0;
+		} else {
+			answerers++;
+			for (int i = 0; i < strlen(line); i++)
+			{
+				if ('a' <= line[i] && line[i] <= 'z')
+				{
+					answered[line[i] - 'a']++;
+				}
+			}
+		}
+	} 
 	
+	total_one += cond_sum(answered, 26, 1);
+	total_two += cond_sum(answered, 26, answerers);
+	init_table(answered, 26, 0);
+	
+	printf("Part 1 : %d\n", total_one);
+	printf("Part 2 : %d\n", total_two);
+
 	fclose(file);
 	return 0;
 }
